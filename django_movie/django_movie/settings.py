6e6b47e428c657f5f107becb6443bb690ff8b678
zim <zim@localhost>
2021-12-20 16:51:30 +0200

    django-filter фильтрация фильмов по годам и жанрам

--- django_movie/django_movie/settings.py
+++ django_movie/django_movie/settings.py
@@ -29,6 +29,7 @@ INSTALLED_APPS = [
     'rest_framework',
     'ckeditor',
     'ckeditor_uploader',
+    'django_filters',
     
     
     'movies',
@@ -194,4 +195,12 @@ CKEDITOR_CONFIGS = {
             'elementspath'
         ]),
     }
+}
+
+
+
+REST_FRAMEWORK = {
+    'DEFAULT_FILTER_BACKENDS': (
+        'django_filters.rest_framework.DjangoFilterBackend',
+    ),
 }
