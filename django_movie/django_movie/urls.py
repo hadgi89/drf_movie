b6f9024acc9742c67c5d263da88f5341c794d72e
zim <zim@localhost>
2021-12-21 15:29:24 +0200

    djoser рег-ция, авт-ция, отправка email с под-ием

--- django_movie/django_movie/urls.py
+++ django_movie/django_movie/urls.py
@@ -1,3 +1,5 @@
+"""django_movie URL Configuration
+"""
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
@@ -7,6 +9,9 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls')),
     path('ckeditor/', include('ckeditor_uploader.urls')),
+    path('auth/', include('djoser.urls')),
+    path('auth/', include('djoser.urls.authtoken')),
+    path('auth/', include('djoser.urls.jwt')),
     path('api/v1/', include('movies.urls')),
 ]
 
