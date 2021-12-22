db06ae9d5b5b16a3e4067043877106a8e132c561
zim <zim@localhost>
2021-12-22 14:20:48 +0200

    Создание компонентов и router vue

---
+++ vue-movie/src/App.vue
@@ -0,0 +1,20 @@
+<template>
+    <div id="app">
+        <Nav />
+        <router-view/>
+        <Footer />
+    </div>
+</template>
+
+<style>
+    @import "assets/css/bootstrap.css";
+    @import "assets/css/font-awesome.css";
+    @import "assets/css/style.css";
+</style>
+<script>
+    import Nav from "./components/Nav";
+    import Footer from "./components/Footer"
+    export default {
+        components: {Nav, Footer}
+    }
+</script>
